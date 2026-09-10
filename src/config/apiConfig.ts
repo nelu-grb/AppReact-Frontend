995b2d2b53c46ea6f47c3dc0e9e973065110bfd0
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-10 11:07:10 -0300

    little update apiconfig

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseURL: https://hyfvjy63q3.execute-api.us-east-1.amazonaws.com
+  baseURL: 'https://hyfvjy63q3.execute-api.us-east-1.amazonaws.com',
   scopes: [
    'api://ed8a85ef-f2d4-48c5-a17e-b69abfc4694e/OT.create',
   ],
